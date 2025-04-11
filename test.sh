9c384cc38e76845d84c5290752ac90d48bb6ffe1
Francesco Andreace <francesco.andreace@gmail.com>
2025-04-11 10:04:56 +0200

    test adjourned

--- test.sh
+++ test.sh
@@ -25,38 +25,38 @@ echo "Disabling HYPERTREADING for more accurate time estimation"
 sudo sh -c 'echo off > /sys/devices/system/cpu/smt/control'
 
 
-# for i in {1..200} #10
-# do
+for i in {1..200} #10
+do
 
-#     a=$(( RANDOM % 989 + 11 ))  # Random integer between 11 and 999 inclusive
+    a=$(( RANDOM % 989 + 11 ))  # Random integer between 11 and 999 inclusive
 
-#     # Calculate 'b' constraints: 6 < b < a and b < 100
-#     b_min=2
-#     b_max=$(( a - 1 ))
-#     if (( b_max > 31 )); then
-#         b_max=31
-#     fi
+    # Calculate 'b' constraints: 6 < b < a and b < 100
+    b_min=2
+    b_max=$(( a - 1 ))
+    if (( b_max > 31 )); then
+        b_max=31
+    fi
 
-#     # Ensure b_min does not exceed b_max
-#     if (( b_max < b_min )); then
-#         b=$b_min
-#     else
-#         b=$(( RANDOM % (b_max - b_min + 1) + b_min ))
-#     fi
-#     sequence=$(generate_sequence 10000)
-#     # Execute the test command with the generated parameters
-#     echo "./test \"$sequence\" $a $b"
-#     ./test "$sequence" $a $b 0
-#     # ./bin/syncmer_tree ./bin/csyncmer "$seqfile" $a $b
-# done
+    # Ensure b_min does not exceed b_max
+    if (( b_max < b_min )); then
+        b=$b_min
+    else
+        b=$(( RANDOM % (b_max - b_min + 1) + b_min ))
+    fi
+    sequence=$(generate_sequence 10000)
+    # Execute the test command with the generated parameters
+    echo "./test \"$sequence\" $a $b"
+    ./test "$sequence" $a $b 0
+    # ./bin/syncmer_tree ./bin/csyncmer "$seqfile" $a $b
+done
 
 
-sequence=CACACAGA
-a=5
-b=2
+# sequence=CACACAGA
+# a=5
+# b=2
 
-echo "./test \"$sequenceN\" $a $b"
-./test "$sequenceN" $a $b 0
+# echo "./test \"$sequenceN\" $a $b"
+# ./test "$sequenceN" $a $b 0
 
 echo "All tests OK."
 
